8b97df0a11ac49b47a21e763f1b5e1cb2a54722c
Bhavesh Kumar <er.bhavesh@live.com>
2018-08-30 15:41:38 +0100

    Implemented START STOP qualifiers support for Splunk SPL query

--- stix2patterns_translator/search_platforms/splunk_search.py
+++ stix2patterns_translator/search_platforms/splunk_search.py
@@ -1,4 +1,6 @@
 import logging
+import re
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -32,24 +34,58 @@ class SplunkSearchTranslator:
         self.object_scoper = object_scoper
         self._pattern_prefix = "|where"  # How should the final SPL query string start.  By default, use '|where'
 
-    def translate(self, expression):
+    def translate(self, expression, qualifier=None):
         """ This is the worker method for the translation. It can be passed any of the STIX2 AST classes and will turn
             them in to strings. It's called recursively and then the results are composed into the full query. """
-
         if isinstance(expression, Pattern):
             # Note: The following call to translate might alter the value of self._pattern_prefix.
-            expr = self.translate(expression.expression)
-
+            expr = self.translate(expression.expression, qualifier=qualifier)
             return "{prefix} {expr}".format(prefix=self._pattern_prefix, expr=expr)
         elif isinstance(expression, ObservationExpression):
             translator = _ObservationExpressionTranslator(expression, self.dmm, self.object_scoper)
-            return translator.translate(expression.comparison_expression)
+            translated_query_str = translator.translate(expression.comparison_expression)
+            if qualifier is not None:
+                # start time pattern
+                st_pattern = r"(START'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z')"
+                # stop time pattern
+                et_pattern = r"(STOP'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z')"
+                # find start and stop time from qualifier string
+                st_arr = re.findall(st_pattern, qualifier)
+                et_arr = re.findall(et_pattern, qualifier)
+                # replace START, STOP and single quotes with empty char in date string
+                earliest =  re.sub(r"(START|')", '', st_arr[0] if st_arr else "")
+                latest   =  re.sub(r"(STOP|')", '', et_arr[0] if et_arr else "")
+                
+                stix_date_format = "%Y-%m-%dT%H:%M:%Sz"
+                splunk_date_format = "%Y-%m-%dT%H:%M:%S.%f" 
+                
+                earliest_obj = datetime.strptime(earliest, stix_date_format)
+                # remove 3 trailing digits from date string to match splunk date format # 2018-08-15T13:01:04.000Z
+                earliest_dt = earliest_obj.strftime(splunk_date_format)[:-3]+'Z'
+                
+                latest_obj = datetime.strptime(latest, stix_date_format)
+                latest_dt = latest_obj.strftime(splunk_date_format)[:-3]+'Z'
+                # prepare splunk SPL query
+                return "{query_string} |earliest={earliest} |latest={latest}".format(query_string=translated_query_str, 
+                                                                                     earliest=earliest_dt,
+                                                                                     latest=latest_dt)
+            else:
+                return "{query_string}".format(query_string=translated_query_str)
         elif isinstance(expression, CombinedObservationExpression):
             combined_expr_format_string = self.implemented_operators[expression.operator]
             if expression.operator == ObservationOperators.FollowedBy:
                 self._pattern_prefix = "|eval"
             return combined_expr_format_string.format(expr1=self.translate(expression.expr1),
                                                       expr2=self.translate(expression.expr2))
+        
+        elif hasattr(expression, 'qualifier') and hasattr(expression, 'observation_expression'):
+            if isinstance(expression.observation_expression, CombinedObservationExpression):
+                expr_format_string = self.implemented_operators[expression.observation_expression.operator]
+                # qualifier only needs to be passed into the parse expression once since it will be the same for both expressions
+                return expr_format_string.format(expr1=self.translate(expression.observation_expression.expr1),
+                                                 expr2=self.translate(expression.observation_expression.expr2, expression.qualifier))
+            else:
+               return self.translate(expression.observation_expression, expression.qualifier)
         else:
             raise NotImplementedError("Comparison type not implemented")
 
